ea5e8cbbdbe054ce146fe12b139be370a14d4ea0
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-11-25 11:55:14 +0100

    risultati

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,20 @@
+import styles from '../style/Results.module.css';
+import SingleResult from './SingleResult';
+
 const Results = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Results</h1>
+            <div className={styles.resultsContainer}>
+               <SingleResult /> 
+               <SingleResult /> 
+               <SingleResult /> 
+               <SingleResult /> 
+            </div>
+            
         </div>
     )
 }
 
 export default Results;
+ 
---
+++ src/components/SingleResult.js
@@ -0,0 +1,12 @@
+import styles from '../style/SingleResult.module.css';
+
+const SingleResults = () => {
+    return (
+        <div className={styles.container}>
+            <p>singolo risultato</p>
+        </div>
+    )
+}
+
+export default SingleResults;
+ 
