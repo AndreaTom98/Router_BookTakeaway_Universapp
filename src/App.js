f3bf41e4eae78b37aed20b22ce13700cdf04157f
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-11-26 12:01:30 +0100

    intro axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {useEffect} from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import Results from './components/Results';
+import axios from 'axios';
 
 function App() {
+
+  const fetchData = async () => {
+    const myData = await axios.get('https://www.googleapis.com/books/v1/volumes?q=harry');
+    console.log(myData);
+  }
+  useEffect(fetchData, [])
+
   return (
     <div>
       <Header />
